72b8df318cafa6bebe98e2186e434fcc3a522fca
john rojas <johnaroj@hotmail.com>
2023-09-27 13:20:25 +0200

    last changes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,23 +1,39 @@
-import React from 'react'
-import { greatVibes } from '../styles/fonts'
-import { FaHeart } from 'react-icons/fa'
+import React from "react";
+import { greatVibes } from "../styles/fonts";
+import { FaHeart } from "react-icons/fa";
 
-type Props = {}
+type Props = {};
 
-function Footer({ }: Props) {
-    return (
-        <section className="mb-4 py-20 flex flex-col justify-center items-center bg-[rgba(207,207,207,1)]">
-            <h1 className={`${greatVibes.className} text-7xl text-center py-10 text-white`}>Thank You for being a part of our new beginning</h1>
-            <div className='flex items-center justify-center space-x-5 my-4 pb-10'><div className='w-[60px] h-0.5 bg-[#B76E78]'></div><FaHeart className='animate-bounce' /><div className='w-[60px] h-0.5 bg-[#B76E78]'></div></div>
-            <div className="relative flex w-full mt-32">
-                <p className="text-white w-1/2 text-right pr-3">info@rojascamargowedding.com</p>
-                <span className="absolute left-1/2">|</span>
-                <p className="text-white w-1/2 text-left pl-3">+5999 699-7796</p>
-            </div>
-            <p className="m-0 text-center">© <a className="text-[#B76E78]" href="info@rojascamargowedding.info">RojasCamargowedding</a> Designed by <a className="text-[#B76E78]" href="https://www.iamjohnrojas.com/">DVLP</a>
-            </p>
-        </section>
-    )
+function Footer({}: Props) {
+  return (
+    <section className="mb-4 py-20 flex flex-col justify-center items-center bg-[rgba(207,207,207,1)]">
+      <h1
+        className={`${greatVibes.className} text-7xl text-center py-10 text-white`}
+      >
+        Thank You for being a part of our new beginning
+      </h1>
+      <div className="flex items-center justify-center space-x-5 my-4 pb-10">
+        <div className="w-[60px] h-0.5 bg-[#B76E78]"></div>
+        <FaHeart className="animate-bounce" />
+        <div className="w-[60px] h-0.5 bg-[#B76E78]"></div>
+      </div>
+      <div className="relative flex items-center justify-center w-full mt-32">
+        <p className="text-white w-1/2 text-right pr-3">
+          info@rojascamargowedding.com
+        </p>
+      </div>
+      <p className="m-0 text-center">
+        ©{" "}
+        <a className="text-[#B76E78]" href="info@rojascamargowedding.info">
+          RojasCamargowedding
+        </a>{" "}
+        Designed by{" "}
+        <a className="text-[#B76E78]" href="https://www.iamjohnrojas.com/">
+          DVLP
+        </a>
+      </p>
+    </section>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/app/components/Rsvp.tsx
+++ src/app/components/Rsvp.tsx
@@ -1,58 +1,114 @@
-"use client"
-import React, { useRef } from 'react'
-import emailjs from '@emailjs/browser';
-import { greatVibes } from '../styles/fonts'
-import { FaHeart } from 'react-icons/fa6'
+"use client";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { greatVibes } from "../styles/fonts";
+import { FaHeart } from "react-icons/fa6";
 
-type Props = {}
+type Props = {};
 
-function Rsvp({ }: Props) {
-    const form = useRef<HTMLFormElement>(null);
-    const sendEmail = (e: any) => {
-        e.preventDefault();
-        const currentForm = form.current;
-        if (currentForm == null) return;
+function Rsvp({}: Props) {
+  const form = useRef<HTMLFormElement>(null);
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+    const currentForm = form.current;
+    if (currentForm == null) return;
 
-        emailjs.sendForm('service_m746hxr', 'template_vbe0zne', currentForm, 'nXYt1NFwpK9Kh65oR')
-            .then((result) => {
-                console.log(result.text);
-                form.current?.reset();
-            }, (error) => {
-                console.log(error.text);
-            });
-    };
-    return (
-        <div className='h-screen flex flex-col max-w-6xl px-10 pt-10 mx-auto items-center'>
-            <div className="mb-4 pt-20 flex flex-col justify-center items-center">
-                <h6 className="uppercase text-[#B76E78] text-md mb-3 font-semibold tracking-widest">RSVP</h6>
-                <h1 className={`${greatVibes.className} text-5xl text-center`}>Please r.s.v.p here before December 20th 2023</h1>
-                <div className='flex items-center justify-center space-x-5 my-4'><div className='w-[60px] h-0.5 bg-[#B76E78]'></div><FaHeart className='animate-bounce' /><div className='w-[60px] h-0.5 bg-[#B76E78]'></div></div>
-                <form ref={form} onSubmit={sendEmail} className='max-w-xl md:max-w-2xl lg:max-w-4xl w-full flex flex-col gap-4'>
-                    <div className='flex w-full flex-col md:flex-row gap-4'>
-                        <input name="name" className='w-full py-4 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:ring-0 focus:shadow-outline bg-[#EDF5F7] border-0' type='text' placeholder='Your Name' required />
-                        <input name="email" className='w-full py-4 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:ring-0 focus:shadow-outline bg-[#EDF5F7] border-0' type='text' placeholder='Your Email' required />
-                    </div>
-                    <div className="flex w-full flex-col md:flex-row gap-4">
-                        <select name="guests" className='w-full md:w-1/2 bg-[#EDF5F7] py-4 px-3 text-gray-700 leading-tight focus:ring-0 focus:outline-yellow-500 focus:shadow-outline border-0' required>
-                            <option value="0">Number of guest (Only if invited)</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                        </select>
-                        <select name="attendees" className='w-full md:w-1/2 bg-[#EDF5F7] py-4 px-3 text-gray-700 leading-tight focus:ring-0 focus:outline-yellow-500 focus:shadow-outline border-0' required>
-                            <option value="Attending">I&apos;m Attending</option>
-                            <option value="Attending only ceremony">Attending only ceremony</option>
-                            <option value="Attending only at the wedding party">Attending only at the wedding party</option>
-                        </select>
-                    </div>
-
-                    <textarea name="message" className='w-full bg-[#EDF5F7] py-2 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:ring-0 focus:shadow-outline border-0' placeholder='Your Message' rows={6} />
-                    <button type="submit" className='bg-[#B76E78] py-4 px-3 text-white'>Submit</button>
-                </form>
-            </div>
+    emailjs
+      .sendForm(
+        "service_m746hxr",
+        "template_vbe0zne",
+        currentForm,
+        "nXYt1NFwpK9Kh65oR"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current?.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="h-screen flex flex-col max-w-6xl px-10 pt-10 mx-auto items-center">
+      <div className="mb-4 pt-20 flex flex-col justify-center items-center">
+        <h6 className="uppercase text-[#B76E78] text-md mb-3 font-semibold tracking-widest">
+          RSVP
+        </h6>
+        <h1 className={`${greatVibes.className} text-5xl text-center`}>
+          Please r.s.v.p here before December 1st 2023
+        </h1>
+        <div className="flex items-center justify-center space-x-5 my-4">
+          <div className="w-[60px] h-0.5 bg-[#B76E78]"></div>
+          <FaHeart className="animate-bounce" />
+          <div className="w-[60px] h-0.5 bg-[#B76E78]"></div>
         </div>
-    )
+        <div className="italic text-slate-600 mb-4">
+          Please note you cannot bring other family members, friends or plus
+          ones at our wedding.
+        </div>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="max-w-xl md:max-w-2xl lg:max-w-4xl w-full flex flex-col gap-4"
+        >
+          <div className="flex w-full flex-col md:flex-row gap-4">
+            <input
+              name="name"
+              className="w-full py-4 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:ring-0 focus:shadow-outline bg-[#EDF5F7] border-0"
+              type="text"
+              placeholder="Your Name"
+              required
+            />
+            <input
+              name="email"
+              className="w-full py-4 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:ring-0 focus:shadow-outline bg-[#EDF5F7] border-0"
+              type="text"
+              placeholder="Your Email"
+              required
+            />
+          </div>
+          <div className="flex w-full flex-col md:flex-row gap-4">
+            <select
+              name="guests"
+              className="w-full md:w-1/2 bg-[#EDF5F7] py-4 px-3 text-gray-700 leading-tight focus:ring-0 focus:outline-yellow-500 focus:shadow-outline border-0"
+              required
+            >
+              <option value="0">Number of guest (Only if invited)</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+            <select
+              name="attendees"
+              className="w-full md:w-1/2 bg-[#EDF5F7] py-4 px-3 text-gray-700 leading-tight focus:ring-0 focus:outline-yellow-500 focus:shadow-outline border-0"
+              required
+            >
+              <option value="Attending">I&apos;m Attending</option>
+              <option value="Attending only ceremony">
+                Attending only ceremony
+              </option>
+              <option value="Attending only at the wedding party">
+                Attending only at the wedding party
+              </option>
+            </select>
+          </div>
+
+          <textarea
+            name="message"
+            className="w-full bg-[#EDF5F7] py-2 px-3 text-gray-700 leading-tight focus:outline-yellow-500 focus:ring-0 focus:shadow-outline border-0"
+            placeholder="Your Message"
+            rows={6}
+          />
+          <button type="submit" className="bg-[#B76E78] py-4 px-3 text-white">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Rsvp
+export default Rsvp;
